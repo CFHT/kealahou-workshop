ffcb02a25a27366e13146dd26c235006a8f62b27
cusher <30704914+cusher@users.noreply.github.com>
2026-03-02 18:03:44 -1000

    Update for API release 1

--- README.md
+++ README.md
@@ -1,38 +1,34 @@
-# Kealahou Workshop
-
-## Kealahou API Examples
-There are some brief examples of accessing the API using cURL in [curl_examples.md](curl_examples.md).
-
-For a more thorough example of walking through the steps of using the API, in Python, see [example.py](example.py).
-
-To run the examples yourself, make sure to set up an access token first,
-as described [below](#Access-Token).
+# Kealahou API Workshop
 
 ## Kealahou Development Details
 
-### Kealahou Staging (Test) Session
+### Kealahou Staging (Test) Environment
 
-URL: **[https://hou-stage.cfht.hawaii.edu](https://hou-stage.cfht.hawaii.edu)**
+Web UI URL: **https://hou-stage.cfht.hawaii.edu**
 
 API URL: **https://api-stage.cfht.hawaii.edu**
 
-### API Documentation
+### AEON+ Test Account
 
-URL: **[https://swagger.cfht.hawaii.edu](https://swagger.cfht.hawaii.edu)**
-
-Note, the API is undergoing ongoing cleanup and improvements.
-This should be considered a preview version, where any and all pieces are subject to change.
-
-- `/programs/lite` endpoint not listed in the docs, lists a user's programs. (Plan to move it to `/programs`.)
-
-
-### Test Account:
-
+This account can be used to test API interactions:
 - User: `aeontest`
 - Password: *[see CFHT staff]*
-- Access Token: *see [below](#Access-Token)*
 
-This account can be used to test API interactions. See examples above.
+### Program IDs
+
+The test account has full access to the following science programs:
+
+- 25BE25
+  - MegaCam (imaging instrument)
+  - Existing target data
+  - Existing observing template (OT) data
+  - Existing observing group (OG) data
+  - Existing exposure data
+- 25BE30
+  - SPIRou (spectrograph)
+  - Existing observing template (OT) data
+
+## Kealahou API
 
 ### Access Token
 
@@ -40,20 +36,33 @@ An access token can be used to allow programmatic access to the API without pote
 These can be generated on the [Manage Tokens](https://hou-stage.cfht.hawaii.edu/account/token-manager) page.
 Note that the token will only be visible to you when you first generate it, so make sure to save it somewhere secure.
 
-### Program IDs
+The examples in this project will assume this token has been saved into a file in this directory named `.access_token`.
 
-- 25BE25 (MegaCam, imaging instrument)
-- 25BE30 (SPIRou, spectrograph)
+### User Workflow
 
-There is existing target and observing template (OT) data in both programs. 25BE25 also has observing group (OG) and exposure data.
-Note that the API endpoint for the program list is not currently available in the API documentation. See [above](#API-Documentation).
+1. A user is granted time on CFHT through the standard CFHT TAC process, or through DDT.
+2. The user creates an API access token, unless they have an existing one.
+3. The user pre-configures the Observing Templates within the K2 interface for their observing program.
+4. The user enters their observation into AEON, which submits target and Observing Group data through the Kealahou API.
+5. This observation is executed by CFHT.
 
-## Questions for the AEON/LCO Team
+### Current Documentation
 
-"Do we automatically trigger the creation of OGs (the encapsulated observation construct), or have a UI on the AEON side for this? How do other observatories handle this for AEON?"
+A Swagger page of the completed API endpoints is available in the project: [api.swagger.json](api.swagger.json).
 
-"Do we need to store any AEON-specific target or observation metadata that is not already included in Kealahou?"
+The original [proto files](proto) can also be viewed for a more direct view of documentation for each field.
 
-"Are there any concerns about how our token-based authentication scheme would match up with their UI, and also about how multi-program users are handled at other observatories – does AEON have a scheme for such cases?"
+### Full API Documentation (Under Construction)
 
-"Regarding error-handling - does AEON have a preferred method of doing so, or specific implementations, protocols, and/or syntax they require?"
+URL: **https://swagger-stage.cfht.hawaii.edu/**
+
+Note, the API is undergoing ongoing cleanup and improvements.
+This should be considered a preview version, where any and all pieces are subject to change.
+
+### API Usage Examples
+
+To run these examples yourself, make sure to set up an access token first, as described [above](#Access-Token).
+
+There are some brief examples of accessing the API using cURL in [curl_examples.md](curl_examples.md).
+
+For a more thorough example of walking through the steps of using the API, in Python, see [example.py](example.py).
--- example.py
+++ example.py
@@ -1,4 +1,6 @@
-'''
+#!/usr/env/bin python3
+
+"""
 Examples of using the Kealahou API for PI program management.
 All API interactions in these examples should be considered as non-final design, and subject to change.
 
@@ -6,7 +8,7 @@ An access token is necessary to run these examples. See the README for details.
 
 Note that, for real world usage, a library such as requests (https://requests.readthedocs.io/)
 is better suited for submitting HTTP requests than the stock Python urllib.
-'''
+"""
 
 import json
 import random
@@ -34,10 +36,11 @@ except OSError:
     raise
 
 
-'''
-Submit a request to the Kealahou API, and condense any errors into a consistent error message format.
-'''
 def api_request(endpoint, data=None, method='GET'):
+    """
+    Submit a request to the Kealahou API, and condense any errors into a consistent error message format.
+    """
+
     if data is None:
         data = {}
     headers = {
@@ -50,66 +53,121 @@ def api_request(endpoint, data=None, method='GET'):
     try:
         raw_response = urlopen(request)
     except HTTPError as e:
-        response = json.loads(e.read().decode('utf-8'))
+        http_error = str(e)
+        try:
+            response = json.loads(e.read().decode('utf-8'))
+        except json.JSONDecodeError:
+            response = dict()
     else:
         response = json.loads(raw_response.read().decode('utf-8'))
 
     if response.get('success'):
         return response
     elif response.get('error') and response['error']['messages']:
-        if http_error:
-            raise Exception(f"API request failed ({http_error}): {', '.join(response['error']['messages'])}")
-        else:
-            raise Exception(f"API request failed: {', '.join(response['error']['messages'])}")
+        raise Exception(f"API request failed: {', '.join(response['error']['messages'])}")
     elif http_error:
         raise Exception(f"API request failed ({http_error})")
     else:
         raise Exception('API request failed, but no error message was given')
 
 
-def k2_example_walkthrough(program_token: str, instrument: INSTRUMENT):
-    print(f'Program {program_token} ({instrument})')
-    print()
+class EntityCrudApi[T]:
+    """
+    Wrapper around the Kealahou API CRUD methods for a given program and entity type.
+    """
 
-    # List observing templates
-    response = api_request(f'/programs/{program_token}/observing-templates')
-    print(f'All observing templates for {program_token}:')
-    for ot in response['observing_template']:
-        print(f"OT{ot['label']} - {ot['name']} [token = {ot['token']}]")
-    print()
-    first_ot = response['observing_template'][0]  # Save for later example
+    def __init__(self, program_token: str, entity_type: str):
+        self.path_prefix = f'/programs/{program_token}/{entity_type}'
 
-    # List targets
-    response = api_request(f'programs/{program_token}/targets')
-    print(f'All targets for {program_token}:')
-    for target in response['target']:
-        print(f"T{target['label']} - {target['name']} [token = {target['token']}]")
-    print()
+    def list(self) -> list[T]:
+        api_response = api_request(f'{self.path_prefix}')
+        return api_response['entity']
 
-    # Invalid new target data
-    new_target = {
+    def show(self, token: str) -> T:
+        api_response = api_request(f'{self.path_prefix}/{token}')
+        return api_response['entity']
+
+    def create_or_update(self, entity: T, request_aux_data: dict = {}) -> dict:
+        version = entity.get('version', None)
+        lock_version = {
+            'value': version,
+        } if version else None
+        api_response = api_request(f'{self.path_prefix}/{entity['token']}', method='PUT', data={
+            'entity': entity,
+            'lock_version': lock_version,  # Use version-locking to prevent accidental overwrites
+            **request_aux_data,
+        })
+        return api_response['entity']
+
+    def delete(self, entity_token: str):
+        api_request(f'{self.path_prefix}/{entity_token}', method='DELETE')
+
+
+def example_fixed_target(program_token: str, instrument: INSTRUMENT):
+    return {
         'token': f'{program_token}-{random.randint(1000000000, 9999999999)}',
         'name': 'My new fancy target!',
         'fixed_target': {
             'coordinate': {
-                'ra': 'twenty point seven degrees',  # Wrong type!
+                'ra': random.uniform(0, 359.9999),
                 'dec': random.uniform(-90, 90),
             },
             'proper_motion': {},
+            'estimated_radial_velocity_kmps': {'value': 234.0} if instrument is 'SPIROU' else None,
         },
-        'target_magnitudes': {
+        'magnitude': {
         },
-        'reference_equinox_julian_years': 2000.0,
+        'temperature_effective': 1234.5,
         'standard_star': False,
         'pointing_offset_token': f'00AZ00-PO+{instrument}+1',
     }
 
+
+def example_moving_target(program_token: str, instrument: INSTRUMENT):
+    return {
+        'token': f'{program_token}-{random.randint(1000000000, 9999999999)}',
+        'name': 'My new moving target!',
+        'moving_target': {
+            'ephemeris_point': [{
+                'mjd': 61041.0 + i,
+                'coordinate': {
+                    'ra': random.uniform(0, 359.9999),
+                    'dec': random.uniform(-90, 90),
+                },
+            } for i in range(0, 5)],
+        },
+        'magnitude': {
+        },
+        'temperature_effective': 1234.5,
+        'standard_star': False,
+        'pointing_offset_token': f'00AZ00-PO+{instrument}+1',
+    }
+
+
+def target_api_examples(program_token: str, instrument: INSTRUMENT):
+    """
+    Complete API examples for Kealahou targets.
+    """
+    print(f'Running through target API examples for {program_token}...')
+    print()
+    target_api = EntityCrudApi[dict](program_token, 'targets')
+
+    # List targets
+    targets = target_api.list()
+    print(f'All targets for {program_token}:')
+    for target in targets:
+        print(f"T{target['label']} - {target['name']} [token = {target['token']}]")
+    print()
+
+    new_target = example_fixed_target(program_token, instrument)
+
+    # Invalid new target data, value is wrong type
+    new_target['fixed_target']['coordinate']['ra'] = 'twenty point seven degrees'
+
     # Attempt to create invalid target
     try:
         print('Trying to create target...')
-        api_request(f"programs/{program_token}/targets/{new_target['token']}", method='PUT', data={
-            'target': new_target,
-        })
+        target_api.create_or_update(new_target)
     except Exception as e:
         print(f'Target creation failed - {e}')
         print()
@@ -120,9 +178,7 @@ def k2_example_walkthrough(program_token: str, instrument: INSTRUMENT):
     # Attempt again with out-of-range value
     try:
         print('Trying to create target...')
-        api_request(f"programs/{program_token}/targets/{new_target['token']}", method='PUT', data={
-            'target': new_target,
-        })
+        target_api.create_or_update(new_target)
     except Exception as e:
         print(f'Target creation failed - {e}')
         print()
@@ -133,30 +189,93 @@ def k2_example_walkthrough(program_token: str, instrument: INSTRUMENT):
     # Attempt again, only missing instrument-specific fields
     try:
         print('Trying to create target with instrument validation...')
-        api_request(f"programs/{program_token}/targets/{new_target['token']}", method='PUT', data={
-            'target': new_target,
-            'instrument': instrument,  # Specify instrument to perform extra validation checks
-        })
+        # Specify instrument to perform extra validation checks
+        target_api.create_or_update(new_target, {'instrument': instrument})
     except Exception as e:
         print(f'Target creation failed - {e}')
         print()
 
     # Add instrument-specific data
-    new_target['target_magnitudes'] = {
+    new_target['magnitude'] = {
         required_mag_by_instrument[instrument]: {
             'value': 10.0,
         },
     }
 
-    # Successfully create new target
-    response = api_request(f"programs/{program_token}/targets/{new_target['token']}", method='PUT', data={
-        'target': new_target,
-        'instrument': instrument,  # Specify instrument to perform extra validation checks
-    })
-    target = response['target']
+    # Successfully create new target with instrument-specific checks
+    target = target_api.create_or_update(new_target, {'instrument': instrument})
     print(f"Created target T{target['label']} - {target['name']} [token = {target['token']}] "
           f"(version={target['version']})")
     print()
+
+    # Update target
+    update_target = target
+    update_target['standard_star'] = True
+    target = target_api.create_or_update(update_target, {'instrument': instrument})
+    print(f"Updated T{target['label']} - {target['name']} [token = {target['token']}]  (version={target['version']})")
+    print()
+
+    # Attempt to update target again with stale version
+    update_target['standard_star'] = False
+    try:
+        target_api.create_or_update(update_target, {'instrument': instrument})
+    except Exception as e:
+        print(f'Target update failed - {e}')
+        print()
+
+    # Fetch single target
+    target = target_api.show(new_target['token'])
+    print(f"Fetched T{target['label']} - {target['name']} [token = {target['token']}] (version={target['version']})")
+    print()
+
+    # Delete the new (unobserved) target
+    target_api.delete(target['token'])
+    print(f"Deleted target T{target['label']} - {target['name']} [token = {target['token']}]")
+    print()
+
+    try:
+        target_api.show(target['token'])
+    except Exception as e:
+        print(f'Target show failed - {e}')
+        print()
+
+    moving_target = example_moving_target(program_token, instrument)
+    moving_target['magnitude'] = {
+        required_mag_by_instrument[instrument]: {
+            'value': 10.0,
+        },
+    }
+
+    # Create moving target
+    moving_target = target_api.create_or_update(moving_target, {'instrument': instrument})
+    print(f"Created target T{moving_target['label']} - {moving_target['name']} [token = {moving_target['token']}] "
+          f"(version={moving_target['version']})")
+    print()
+
+    # Delete the new (unobserved) moving target
+    target_api.delete(moving_target['token'])
+    print(f"Deleted target T{moving_target['label']} - {moving_target['name']} [token = {moving_target['token']}]")
+    print()
+
+
+def k2_example_walkthrough(program_token: str, instrument: INSTRUMENT):
+    """
+    Walkthrough of work-in-progress API examples for full K2 program management.
+    """
+    print(f'K2 examples for program {program_token} ({instrument})')
+    print()
+
+    # List observing templates
+    response = api_request(f'/programs/{program_token}/observing-templates')
+    print(f'All observing templates for {program_token}:')
+    for ot in response['observing_template']:
+        print(f"OT{ot['label']} - {ot['name']} [token = {ot['token']}]")
+    print()
+    first_ot = response['observing_template'][0]  # Save for later example
+
+    # Create new target
+    target_api = EntityCrudApi[dict](program_token, 'targets')
+    target = target_api.create_or_update(example_fixed_target(program_token, instrument))
 
     # Create new basic OG from new target
     new_og = {
@@ -179,25 +298,12 @@ def k2_example_walkthrough(program_token: str, instrument: INSTRUMENT):
     print(f"Created observing group OG{og['label']} [token = {og['token']}]")
     print()
 
-    # Update target
-    update_target = target
-    update_target['standard_star'] = True
-    response = api_request(f"programs/{program_token}/targets/{update_target['token']}", method='PUT', data={
-        'target': update_target,
-        'lock_version': {
-            'value': update_target['version'],  # Use version-locking to prevent accidental overwrites
-        },
-        'instrument': instrument,  # Specify instrument to perform extra validation checks
-    })
-    target = response['target']
-    print(f"Updated T{target['label']} - {target['name']} [token = {target['token']}]  (version={target['version']})")
-    print()
-
-    # Fetch single target
-    response = api_request(f"programs/{program_token}/targets/{new_target['token']}")
-    target = response['target']
-    print(f"Fetched T{target['label']} - {target['name']} [token = {target['token']}] (version={target['version']})")
-    print()
+    # Attempt to delete the target that is used in an observing group
+    try:
+        target_api.show(target['token'])
+    except Exception as e:
+        print(f'Target delete failed - {e}')
+        print()
 
     # Delete the new (unobserved) OG
     api_request(f"observing-groups/{og['token']}", method='DELETE')
@@ -205,23 +311,7 @@ def k2_example_walkthrough(program_token: str, instrument: INSTRUMENT):
     print()
 
     # Delete the new (unobserved) target
-    api_request(f"targets/{target['token']}", method='DELETE')
-    print(f"Deleted target T{target['label']} - {target['name']} [token = {target['token']}]")
-    print()
-
-    # Attempt to delete observed target
-    try:
-        api_request(f"targets/25BE25-1758314224958", method='DELETE')
-    except Exception as e:
-        print(f'Target deletion failed - {e}')
-        print()
-
-    # Attempt to delete observed observing group
-    try:
-        api_request(f"observing-groups/25BE25-1758314360503", method='DELETE')
-    except Exception as e:
-        print(f'Observing group deletion failed - {e}')
-        print()
+    target_api.delete(target['token'])
 
     # List exposures
     response = api_request(f'/programs/{program_token}/exposures')
@@ -229,11 +319,33 @@ def k2_example_walkthrough(program_token: str, instrument: INSTRUMENT):
     for exp in response['exposure']:
         print(f"{exp['obsid']} (OG{exp['observing_group_context']['observing_group_label']}, {exp['target']['name']})")
     print()
+    print()
 
-# List programs
-response = api_request('programs')
-for program in response['entity']:
-    program_data = program['program_data']
-    program_token = program_data['token']
-    instrument = program_data['time_allocation'][0]['instrument']
-    k2_example_walkthrough(program_token, instrument)
+
+def main():
+    # List programs
+    response = api_request('programs')
+    for program in response['entity']:
+        program_data = program['program_data']
+        program_token = program_data['token']
+        instrument = program_data['time_allocation'][0]['instrument']
+        target_api_examples(program_token, instrument)
+        k2_example_walkthrough(program_token, instrument)
+
+    # Attempt to delete an observed target
+    try:
+        api_request(f"targets/25BE25-1758314224958", method='DELETE')
+    except Exception as e:
+        print(f'Target deletion failed - {e}')
+        print()
+
+    # Attempt to delete an observed observing group
+    try:
+        api_request(f"observing-groups/25BE25-1758314360503", method='DELETE')
+    except Exception as e:
+        print(f'Observing group deletion failed - {e}')
+        print()
+
+
+if __name__ == '__main__':
+    main()
